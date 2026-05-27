b029f5da2e0384eae8b490d32b9c03d352067781
Larissa <larissasteffen97@gmail.com>
2026-05-26 21:09:57 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+print('Setup Star')
+pygame.init() #inicialização do pygame
+
+#Para criar uma janela utilizasse a variável window
+print('Setup End')
+window = pygame.display.set_mode(size = (600, 480)) #tamanho da janela 600px por 480px
+
+
+print('Loop Star')
+#Laço para manter a janela aberta
+while True:
+    #Criasse o evento para finalizar a janela
+    #Check for all events
+    for event in pygame.event.get(): #checa e pega os eventos
+        if event.type == pygame.QUIT: #se o tipo do evendo for sair
+            pygame.quit() #CLose Window
+            quit() #encerra a inicialização do pygame
