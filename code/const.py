644493424cff83f64c1935f9752e5e76ab748db7
Larissa <larissasteffen97@gmail.com>
2026-05-27 21:31:07 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	code/game.py
    #	code/menu.py

---
+++ code/const.py
@@ -0,0 +1,17 @@
+from pygame.examples.grid import WINDOW_WIDTH
+#C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT'
+)
+
+#Tamanho imagens
+#W
+WIN_WIDTH = 576
+WIN_WEIGHT = 324
--- code/game.py
+++ code/game.py
@@ -5,6 +5,8 @@ import pygame
 
 from code.menu import Menu
 
+from code.const import WIN_WIDTH
+from code.const import WIN_WEIGHT
 
 #método para abrir a janela
 class Game:
@@ -12,16 +14,9 @@ class Game:
     def __init__(self):
         pygame.init() #inicialização do pygame
         #Para criar uma janela utilizasse a variável window
-        self.window = pygame.display.set_mode(size = (600, 480)) #tamanho da janela 600px por 480px
+        self.window = pygame.display.set_mode(size = (WIN_WIDTH, WIN_WEIGHT)) #tamanho da janela 600px por 480px
 
     def run(self):
-        while True:  #Laço para manter a janela aberta
             menu = Menu(self.window) #primeiro chamar o menu
             menu.run()
             pass
-
-            #Criasse o evento para finalizar a janela / #Check for all events
-            #for event in pygame.event.get(): #checa e pega os eventos
-            #    if event.type == pygame.QUIT: #se o tipo do evendo for sair
-            #        pygame.quit() #CLose Window
-            #        quit() #encerra a inicialização do pygame
